0007fd5fda51d114a355b6cad6274bc7c285e4ae
Soheil <ysoheil@gmail.com>
2013-10-15 15:37:13 -0700

    init project

---
+++ HomerTests/HomerTests.m
@@ -0,0 +1,34 @@
+//
+//  HomerTests.m
+//  HomerTests
+//
+//  Created by Soheil Yasrebi on 10/15/13.
+//  Copyright (c) 2013 Soheil Yasrebi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HomerTests : XCTestCase
+
+@end
+
+@implementation HomerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
